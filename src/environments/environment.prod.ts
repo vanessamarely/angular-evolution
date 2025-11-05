094a8c8cbe503e97b8fd80d91c0de9931f8e3e19
vanessamarely <vanessamarely@gmail.com>
2025-11-05 16:44:40 -0500

    fix api call

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+// Environment configuration for production
+export const environment = {
+  production: true,
+  geminiApiKey: 'AIzaSyCS47zk_jhwhN2EVgaYF0vYOtlyAwWI4sY' // In real production, this should come from build-time env vars
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+// Environment configuration for development
+export const environment = {
+  production: false,
+  geminiApiKey: 'AIzaSyCS47zk_jhwhN2EVgaYF0vYOtlyAwWI4sY'
+};
